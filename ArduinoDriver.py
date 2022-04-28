04995706d1487c5b1bfb976350639da7ddaf5d4d
Vincent Bezold <vincent.bezold@web.de>
2022-04-28 15:58:23 +0200

    ArduinoDriverfunction

--- ArduinoDriver.py
+++ ArduinoDriver.py
@@ -1,15 +1,20 @@
 import serial
 import time
 
-arduino = serial.Serial(port='COM14', baudrate=9600, timeout=1)
 
-def write_read(x):
-    arduino.write(bytes(x, 'utf-8'))
-    time.sleep(0.1)
-    data = arduino.readline()
-    return data
+class ArduinoDriver:
 
-while True:
-    num = input("Enter a number: ") # Taking input from user
-    value = write_read(num)
-    print(value.decode('utf-8')) # printing the values
+    def __init__(self):
+        self.arduino = serial.Serial(port='COM14', baudrate=9600, timeout=1)
+
+
+    def write_read(self):
+        self.arduino.write(bytes('M', 'utf-8'))
+        time.sleep(0.1)
+        data = self.arduino.readline()
+        return data
+
+    # while True:
+    #     num = input("Enter a number: ") # Taking input from user
+    #     value = write_read(num)
+    #     value.decode('utf-8') # printing the values
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from PyQt5.QtWidgets import QMainWindow
 from OphirComDriver import OphirComDriver
 from MainUIWindow import Ui_MainWindow
 # comment to better understand the code
+// python -m PyQt5.uic.pyuic UIWindow.ui -o MainUIWindow.py
 class test:
     def __init__(self):
         self.main_win = QMainWindow()
