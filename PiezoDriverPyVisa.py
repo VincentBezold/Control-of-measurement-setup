e67da476dabe9e21e497b40c7e756fb8a2eb6e9f
Vincent Bezold <vincent.bezold@web.de>
2022-05-09 14:16:50 +0200

    Piezo Driver finished

--- PiezoDriverPyVisa.py
+++ PiezoDriverPyVisa.py
@@ -1,26 +1,22 @@
-# import pyvisa
-# rm = pyvisa.ResourceManager()
-# print(rm.list_resources())
 import pyvisa
 import json, os
 import time
-# import numpy as np
 from datetime import datetime
-
+# https://www.aps.anl.gov/files/APS-Uploads/SECTOR27/Manual%20ANC300%20v3.1.pdf
 
 class PiezoDriver:
     def __init__(self):
-        rm = pyvisa.ResourceManager()
-        self.inst = rm.open_resource('ASRL9::INSTR')
-        self.inst.baud_rate = 38400
+        rm = pyvisa.ResourceManager() # some visa magic
+        self.inst = rm.open_resource('ASRL9::INSTR') # opens port
+        self.inst.baud_rate = 38400 # sets baudrate
                 
     def moveContinuesUp(self, channel):
         time.sleep(1)
-        self.inst.write("setm " + str(channel)+" stp\n")
-        self.inst.write("stepu " + str(channel)+" c\n")
+        self.inst.write("setm " + str(channel)+" stp\n") # sets mode to stp (step)
+        self.inst.write("stepu " + str(channel)+" c\n") # starts continues mode
         time.sleep(2)
         self.stop(str(channel))
-        self.inst.write("setm " + str(channel)+" gnd\n")
+        self.inst.write("setm " + str(channel)+" gnd\n") # set piezos back to ground
 
 
     def moveContinuesDown(self, channel):
@@ -36,8 +32,8 @@ class PiezoDriver:
         
     def stepUp(self, channel, numberSteps):
         self.inst.write("setm " + str(channel)+" stp\n")
-        self.inst.write("stepu " + str(channel) + " " + str(numberSteps) + "\n")
-        self.inst.write("stepw " + str(channel)+"\n")
+        self.inst.write("stepu " + str(channel) + " " + str(numberSteps) + "\n") # starts starts step mode
+        self.inst.write("stepw " + str(channel)+"\n") # waits until steps are done
         self.inst.write("setm " + str(channel)+" gnd\n")
     
     def stepDown(self, channel, numberSteps):
@@ -56,4 +52,3 @@ time.sleep(1)
 test.moveContinuesUp(2)
 
 
-# https://www.aps.anl.gov/files/APS-Uploads/SECTOR27/Manual%20ANC300%20v3.1.pdf
